11a393a23a6f481af99c6abe42f9b176b8b64dbf
surya-kangeyan <suryakangeyan4701@gmail.com>
2024-09-27 12:23:00 -0700

    Remove env.d.ts from tracking

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="@remix-run/node" />
-// .env
-SHOPIFY_API_KEY = "77683c2485bb844041b6c988e109d8c6";
-SHOPIFY_API_SECRET = "649ff13fd4e61b7ac9dc8e7c2394b0b4";
-SHOPIFY_SCOPES ="read_products,write_orders,read_product_listings,write_products";
-SHOPIFY_HOST="http://localhost:60042/"
-RED
